aee3ff2787ac1daffbb1941c55d5170bd6ed9849
y-temp4 <y.temp4@gmail.com>
2021-04-30 19:17:48 +0900

    chore: refactor test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,4 +1,4 @@
-import path from 'path'
+import fs from 'fs'
 import unified from 'unified'
 import remark from 'remark-parse'
 import remark2rehype from 'remark-rehype'
@@ -7,27 +7,35 @@ import stringify from 'rehype-stringify'
 import vfile from 'to-vfile'
 import rehypeTitleFigure from '../src'
 
-function compile(file: string) {
+function compile(file: string): string {
   return unified()
     .use(remark)
     .use(remark2rehype, { allowDangerousHtml: true })
     .use(raw)
     .use(rehypeTitleFigure)
     .use(stringify)
-    .processSync(vfile.readSync(`${path.resolve('')}/tests/fixtures/${file}`))
+    .processSync(vfile.readSync(`${__dirname}/fixtures/${file}`))
     .toString()
+}
+
+function readWantFileSync(file: string): string {
+  return fs
+    .readFileSync(`${__dirname}/fixtures/${file}`, {
+      encoding: 'utf-8',
+    })
+    .trim()
 }
 
 describe('index.ts', () => {
   test('add figcaptions', () => {
     const got = compile('sample.md')
-    const want = compile('sample.html')
+    const want = readWantFileSync('sample.html')
     expect(got).toBe(want)
   })
 
   test('no images', () => {
     const got = compile('no-images.md')
-    const want = compile('no-images.html')
+    const want = readWantFileSync('no-images.html')
     expect(got).toBe(want)
   })
 })
